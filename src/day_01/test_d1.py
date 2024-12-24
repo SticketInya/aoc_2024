90fbf0d9c807a7b3e9cfbbc4503dfd59cd405d55
JPatrik <jokhel.roland@gmail.com>
2024-12-24 11:38:58 +0100

    feat: remove prev solutions; add vscode settings

--- src/day_01/test_d1.py
+++ src/day_01/test_d1.py
@@ -1,17 +1,7 @@
-from .main import calc_distance_between_lists, calc_similarity_score
+from .main import print_title_and_day
 
 
-def test_calc_distance_between_lists():
-    tests = [{"l1": [3, 4, 2, 1, 3, 3], "l2": [4, 3, 5, 3, 9, 3], "output": 11}]
+def test_print_title_and_day():
+    result = print_title_and_day()
 
-    for tt in tests:
-        got = calc_distance_between_lists(tt["l1"], tt["l2"])
-        assert got == tt["output"]
-
-
-def test_calc_similarity_score():
-    tests = [{"l1": [3, 4, 2, 1, 3, 3], "l2": [4, 3, 5, 3, 9, 3], "output": 31}]
-
-    for tt in tests:
-        got = calc_similarity_score(tt["l1"], tt["l2"])
-        assert got == tt["output"]
+    assert result is None
